7ead211b66ffac959b889f845ca3ceae19d3c5ae
ArifSanaullah <ak7896023@gmail.com>
2020-03-11 19:26:24 +0500

    learning merge sort.

---
+++ mergeSort.js
@@ -0,0 +1,31 @@
+
+let arr = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
+
+function merge(left, right) {
+    let arr = [];
+    while (left.length && right.length) {
+        if (left[0] < right[0]) {
+            const n = left.shift();
+            arr.push(n);
+        } else {
+            const n = right.shift();
+            arr.push(n);
+        }
+    }
+
+    return arr.concat(left.slice().concat(right.slice()));
+}
+
+
+function mergeSort(arr) {
+    if (arr.length < 2) return arr;
+
+    const middle = Math.floor(arr.length / 2);
+    const left = arr.slice(0, middle);
+    const right = arr.slice(middle);
+
+    return merge(mergeSort(left), mergeSort(right));
+}
+
+
+console.log(mergeSort(arr));
